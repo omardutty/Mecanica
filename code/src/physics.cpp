99528dc64afbaaaa55d3faec56effc1bed35a805
Unknown <mgisbertlopez@gmail.com>
2018-04-17 11:05:34 +0200

    En proceso momento angular,inercia,etc

--- code/src/physics.cpp
+++ code/src/physics.cpp
@@ -6,7 +6,15 @@
 #include<iostream>
 #include<time.h>
 
-
+glm::mat3 I;
+glm::mat3 lastI{	1,0,0,
+					0,1,0,
+					0,0,1 };
+glm::mat3 LastR = { 1,0,0,
+					0,1,0,
+					0,0,1};
+glm::vec3 tor,AngMoment;
+glm::vec3 LastAngMoment = { 0,0,0 };
 glm::mat4 objMat, rotXMat,rotYMat,rotZMat;
 glm::vec3 gravity = { 0,9.81,0 };
 glm::vec4 position = { (rand() % 9) - 4, (rand() % 9) + 1, (rand() % 9) - 4, 1.f };
@@ -35,7 +43,8 @@ public:
 	static glm::mat3 lastR(glm::mat3 lastR, glm::vec3 w, float dt);
 	static glm::mat3 calcI(glm::mat3 R, float mass, float w, float h, float d);
 	static glm::vec3 angularMoment(glm::vec3 L,float dt, glm::vec3 t);
-	static glm::vec3 w(glm::mat3 R, glm::vec3 L);
+	static glm::vec3 w(glm::vec3 L,glm::mat3 I);
+	static glm::vec3 Torque(glm::vec3 Punto, glm::vec3 Pos, glm::vec3 F);
 
 	RigidBody();
 	~RigidBody();
@@ -66,12 +75,21 @@ glm::mat3 RigidBody::lastR(glm::mat3 R, glm::vec3 w, float dt) {
 }
 
 glm::mat3 RigidBody::calcI(glm::mat3 R, float mass, float w, float h, float d) {
-	glm::mat3 Ibody = { (1 / 12)*mass*(pow(d,2) + pow(h,2)),0,0,0,(1 / 12)*mass*(pow(w,2) + pow(d,2)),0,0,0,(1 / 12)*mass*(pow(w,2) + pow(h,2)) };
+	glm::mat3 Ibody = { (1.f / 12.f)*mass*(pow(d,2) + pow(h,2)),0,0,0,(1.f / 12.f)*mass*(pow(w,2) + pow(d,2)),0,0,0,(1.f / 12.f)*mass*(pow(w,2) + pow(h,2)) };
 	return R*glm::inverse(Ibody)*R;
 }
 
 glm::vec3 RigidBody::angularMoment(glm::vec3 L,float dt, glm::vec3 t) {
 	return L + dt*t;
+}
+glm::vec3 RigidBody::w(glm::vec3 L, glm::mat3 I) {
+	return glm::inverse(I)*L;
+}
+
+glm::vec3 RigidBody::Torque(glm::vec3 Punto, glm::vec3 Pos, glm::vec3 F) {
+	glm::vec3 vector = Punto - Pos;
+	return glm::normalize(vector * F);
+
 }
 
 float a = pow(2, 2);
@@ -135,16 +153,37 @@ void PhysicsInit() {
 	rotXMat = glm::mat4(1, 0, 0, 0, 0, cos(randValX), sin(randValX), 0, 0, -sin(randValX), cos(randValX), 0, 0, 0, 0, 1);
 	rotYMat = glm::mat4(cos(randValY), 0, -sin(randValY), 0, 0, 1, 0, 0, sin(randValY), 0, cos(randValY), 0, 0, 0, 0, 1);
 	rotZMat = glm::mat4(cos(randValZ), sin(randValZ), 0, 0, -sin(randValZ), cos(randValZ), 0, 0, 0, 0, 1, 0, 0, 0, 0, 1);
+	tor = RigidBody::Torque(glm::vec3((rand() % 10) / 10.f, (rand() % 10) / 10.f, (rand() % 10) / 10.f), position, glm::vec3(0, -9.81*mass, 0));
+	AngMoment = RigidBody::angularMoment(glm::vec3(0.f, 0.f, 0.f), 0.f, tor);
+	LastAngMoment = AngMoment;
+	LastR = { 1,0,0,
+		0,1,0,
+		0,0,1 };
+	lastI = { 1,0,0,
+		0,1,0,
+		0,0,1 };
 }
 
 void PhysicsUpdate(float dt) {
+	//Momento Lineal
 	glm::vec4 Mtmp = RigidBody::nextP(llMoment, dt, glm::vec4(0, -9.81*mass, 0, 0));
 	glm::vec4 linealMoment = llMoment;
 	llMoment = Mtmp;
+	//Velocidad
 	glm::vec4 velocity = RigidBody::calcV(linealMoment, mass);
+	//Posicion
 	glm::vec4 tmp = RigidBody::nextX(lastX, dt, velocity);
 	position = lastX;
 	lastX = tmp;
+	//Momento Angular
+	glm::vec3 AnMomnentTmp = RigidBody::angularMoment(LastAngMoment, dt, tor);
+	AngMoment = LastAngMoment;
+	LastAngMoment = AnMomnentTmp;
+	//Inercia
+	glm::mat3 Itmp = RigidBody::calcI(LastR, mass, 1.f, 1.f, 1.f);
+	I = lastI;
+	lastI = Itmp;
+	
 
 	objMat = glm::mat4(1, 0, 0, 0.f,
 		0, 1, 0, 0.f,
